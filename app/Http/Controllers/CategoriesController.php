4707d08062e092ad09a83c202151e5e1c6bb18bb
theClubhou.se <heythere@theclubhou.se>
2017-05-05 11:19:08 -0400

    CategoriesController

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,5 +6,74 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    //
+    //will get list
+    public function index()
+    {
+      //create a query to get a list and receive on the Frontend
+      $catorgies = Category::all();
+
+      return Response::json($catories);
+    }
+
+    //store - takes request param from Frontend
+    public function store(Request $request)
+    {
+      $rules = [
+        'name' => 'required',
+      ];
+
+      $validator = Validator::make(Purifier::clean($request->all()), $rules);
+        if($validator->fails())
+        {
+          return Response::json(['error'=>"Error. Please add a Category."]);
+        }
+
+      $category = new Category;
+
+      $category->name= $request->input('name');
+
+      $category->save();
+
+      //return a response from server to the frontend. Will get either a Success or Error
+      return Response::json(["success" => "Congratulations, You Did It!"]);
+    }
+    //update function - 2 params id & request
+    public function update($id, Request $request)
+    {
+      $rules = [
+        'name' => 'required',
+
+      ];
+
+      $validator = Validator::make(Purifier::clean($request->all()), $rules);
+      if($validator->fails())
+      {
+        return Response::json(['error'=>"ERROR! Category did not update!"])
+      }
+
+      $category = Category::find($id);
+
+      $category->name = $request->input('name');
+
+      return Response::json(['success' => "Category Has Been Updated!"])
+    }
+
+    //shows individual article
+    public function show($id)
+    {
+      $category = Category::find($id);
+
+      return Response::json($category);
+    }
+
+    //delete function to delete a single category
+    public function destory($id)
+    {
+      $category = Category::find($id);
+
+      $category->delete();
+
+      return Response::json(['success' => "Category Deleted!"])
+    }
+
 }
